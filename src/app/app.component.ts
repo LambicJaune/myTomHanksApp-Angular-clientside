de46255d9ec5f4e49bc50cc596cd79e86a088b78
LambicJaune <thehopman@outlook.com>
2025-09-19 10:45:17 +0200

    Fixes standalone component issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,21 @@ import { UserRegistrationFormComponent } from './user-registration-form/user-reg
 import { MatDialog } from '@angular/material/dialog';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    standalone: false,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'myTomHanksApp-Angular-clientside';
+    title = 'myTomHanksApp-Angular-clientside';
 
-  constructor(public dialog: MatDialog) { }
-// This is the function that will open the dialog when the signup button is clicked  
-openUserRegistrationDialog(): void {
-    this.dialog.open(UserRegistrationFormComponent, {
-// Assigning the dialog a width
-    width: '280px'
-    });
-  }
+    constructor(public dialog: MatDialog) { }
+    // This is the function that will open the dialog when the signup button is clicked  
+    openUserRegistrationDialog(): void {
+        this.dialog.open(UserRegistrationFormComponent, {
+            // Assigning the dialog a width
+            width: '280px'
+        });
+    }
 
 }
--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -12,42 +12,44 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 
 
 @Component({
-  selector: 'app-user-registration-form',
-  templateUrl: './user-registration-form.component.html',
-  styleUrls: ['./user-registration-form.component.scss']
+    selector: 'app-user-registration-form',
+    standalone: false,
+    templateUrl: './user-registration-form.component.html',
+    styleUrls: ['./user-registration-form.component.scss']
 })
 export class UserRegistrationFormComponent implements OnInit {
 
-  @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
+    @Input() userData = { Username: '', Password: '', Email: '', Birthday: '' };
 
-/**
- * Called when creating an instance of the class
- * @param fetchApiData 
- * @param dialogRef 
- * @param snackBar 
- */
+    /**
+     * Called when creating an instance of the class
+     * @param fetchApiData 
+     * @param dialogRef 
+     * @param snackBar 
+     */
 
-constructor(
-    public fetchApiData: FetchApiDataService,
-    public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
-    public snackBar: MatSnackBar) { }
+    constructor(
+        public fetchApiData: FetchApiDataService,
+        public dialogRef: MatDialogRef<UserRegistrationFormComponent>,
+        public snackBar: MatSnackBar) { }
 
-ngOnInit(): void {
+    ngOnInit(): void {
+    }
+
+    // This is the function responsible for sending the form inputs to the backend
+    registerUser(): void {
+        this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
+            // Logic for a successful user registration goes here! (To be implemented)
+            this.dialogRef.close(); // This will close the modal on success!
+            console.log(Response);
+            this.snackBar.open(result, 'OK', {
+                duration: 2000
+            });
+        }, (result) => {
+            this.snackBar.open(result, 'OK', {
+                duration: 2000
+            });
+        });
+    }
+
 }
-
-// This is the function responsible for sending the form inputs to the backend
-registerUser(): void {
-    this.fetchApiData.userRegistration(this.userData).subscribe((result) => {
-  // Logic for a successful user registration goes here! (To be implemented)
-     this.dialogRef.close(); // This will close the modal on success!
-     this.snackBar.open(result, 'OK', {
-        duration: 2000
-     });
-    }, (result) => {
-      this.snackBar.open(result, 'OK', {
-        duration: 2000
-      });
-    });
-  }
-
-  }
